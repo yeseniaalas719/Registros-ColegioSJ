970cb8db613e158c35b73bd69c120bb8a8a16347
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-10 20:38:26 -0600

    Creación de ReportesController con LINQ para promedios

---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Registros_ColegioSJ.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registros_ColegioSJ.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly ColegioSJContext _context;
+
+        public ReportesController(ColegioSJContext context)
+        {
+            _context = context;
+        }
+
+        // NOMBRE Y PROMEDIO //
+        public async Task<IActionResult> PromediosEstudiantes()
+        {
+            //CONEXIÓN CON LINQ//
+            var promedios = await _context.Estudiantes
+                .AsNoTracking()
+                .Select(e => new
+                {
+                    Estudiante = e.Nombre + " " + e.Apellido,
+                    Promedio = e.Expedientes.Any() ? e.Expedientes.Average(exp => exp.NotaFinal) : 0
+                })
+                .ToListAsync();
+
+            return View(promedios);
+        }
+    }
+}
