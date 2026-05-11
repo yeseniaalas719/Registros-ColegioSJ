7c4cced3d1912535e667f61ed26ed4e770902ffb
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-10 20:22:26 -0600

    Actualización de Program.cs para conectar SQLite

--- Program.cs
+++ Program.cs
@@ -1,29 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Registros_ColegioSJ.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+// CONTROLADORES Y VISTAS //
+
 builder.Services.AddControllersWithViews();
+
+// INYECCIÓN SQL //
+
+builder.Services.AddDbContext<ColegioSJContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConexionSQLite")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// REVISION O CREACION DE DB //
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ColegioSJContext>();
+       
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocurrió un error al crear la base de datos.");
+    }
+}
+
+//CONFIGURACIÓN DEL PIPELINE DE HTTP //
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
