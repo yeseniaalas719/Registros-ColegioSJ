46506045f61129a67fa65481035361ea840e4acc
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-10 19:22:41 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registros_ColegioSJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
