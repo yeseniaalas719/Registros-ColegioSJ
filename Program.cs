172264e6207aab9dc4027f4d7b109b5db80831e0
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-11 22:22:40 -0600

    Integrar gráfica de estado y optimización del UI de home y secciones

--- Program.cs
+++ Program.cs
@@ -15,15 +15,12 @@ builder.Services.AddDbContext<ColegioSJContext>(options =>
 
 var app = builder.Build();
 
-// REVISION O CREACION DE DB //
-
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     try
     {
         var context = services.GetRequiredService<ColegioSJContext>();
-       
         context.Database.EnsureCreated();
     }
     catch (Exception ex)
@@ -36,20 +33,20 @@ using (var scope = app.Services.CreateScope())
 //CONFIGURACIÓN DEL PIPELINE DE HTTP //
 
 if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.UseRouting();
+    app.UseRouting();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+    app.Run();
