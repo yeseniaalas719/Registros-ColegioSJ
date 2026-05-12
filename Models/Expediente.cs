bcd5c1332c925f23697ac4f801638e1b13137ed9
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-11 19:51:08 -0600

    Resolver error de guardado de calificaciones y agregar validacion

--- Models/Expediente.cs
+++ Models/Expediente.cs
@@ -4,18 +4,19 @@ namespace Registros_ColegioSJ.Models
 {
     public class Expediente
     {
-        [Key]
         public int ExpedienteId { get; set; }
 
-        [Required]
         public int EstudianteId { get; set; }
-        public Estudiante Estudiante { get; set; }
+        public Estudiante? Estudiante { get; set; }
 
-        [Required]
         public int AsignaturaId { get; set; }
-        public Asignatura Asignatura { get; set; }
+        public Asignatura? Asignatura { get; set; }
 
+        // VALIDACION //
+        [Required(ErrorMessage = "La nota final es obligatoria.")]
+        [Range(0, 10, ErrorMessage = "La nota debe estar entre 0.0 y 10.0.")]
         public decimal NotaFinal { get; set; }
-        public string Observaciones { get; set; }
+
+        public string? Observaciones { get; set; }
     }
 }
